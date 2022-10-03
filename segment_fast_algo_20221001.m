ca44d55f6aa2cf27855a96db92b1b01d5926357e
ray-qwer <b07901055@ntu.edu.tw>
2022-10-04 02:44:59 +0800

    little region combine v1; 20221004

--- segment_fast_algo_20221001.m
+++ segment_fast_algo_20221001.m
@@ -3,6 +3,7 @@ img = double(rgb2gray(img));
 
 % fast algorithm
 th = 25;
+min_pixel = ceil(512*512/1000);
 R = zeros(size(img));
 mean_num_list = zeros(1,2); %A: 1, B: 2
 
@@ -54,10 +55,12 @@ for m = 1:sz(1)
                     if R(m-1,n) > R(m,n-1)  % R(m-1,n) combine to R(m,n-1)
                         R(m,n) = R(m,n-1);
                         mean_num_list(R(m,n-1),:) = [new_mean, new_num];
+                        mean_num_list(R(m-1,n),:) = [0,0];
                         R(R == R(m-1,n)) = R(m,n-1);
                     else    % R(m,n-1) combine to R(m-1,n)
                         R(m,n) = R(m-1,n);
                         mean_num_list(R(m-1,n),:) = [new_mean, new_num];
+                        mean_num_list(R(m,n-1),:) = [0,0];
                         R(R == R(m,n-1)) = R(m-1,n);
                     end
                 end    
@@ -79,6 +82,29 @@ for m = 1:sz(1)
             end
         end
     end
+end
+t = mean_num_list(:,2);
+index = find(t<min_pixel & t >0);
+neighbor_matrix = [0,1,0;1,0,1;0,1,0];
+for i = 1:length(index)
+    M = zeros(sz);
+    di_M = zeros(sz);
+    [r,c] = find(R == index(i));
+    M((c-1)*sz(1)+r) = 1;
+    di_M(conv2(M,neighbor_matrix,'same') > 0) = 1;
+    [n_r,n_c] =  find((di_M - M) == 1); % get neighbor
+    u = unique(R((n_c-1)*sz(1)+n_r));
+    for j = length(u):-1:1
+        if mean_num_list(u(j),2) < region_th
+            u(j) = [];
+        end
+    end
+    [min_dist,u_index]=min(abs(mean_num_list(index(i),1) - mean_num_list(u,1)));
+    % update mean, num, region
+    region_index = u(u_index);
+    new_num = mean_num_list(region_index,2) + mean_num_list(index(i),2);
+    mean_num_list(region_index,2) = new_num;
+    R(R == index(i)) = region_index;
 end
 
 for i = 1:r_cnt
