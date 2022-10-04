3bb0fe678285d3f3485646e6a9e01ab63f11e04f
ray-qwer <b07901055@ntu.edu.tw>
2022-10-04 21:39:40 +0800

    corrected; 20221004 2139

--- segment_fast_algo_20221001_color.m
+++ segment_fast_algo_20221001_color.m
@@ -2,7 +2,7 @@ img = double(imread("lena_color.png"));
 
 tic
 % fast algorithm
-th = 25;
+th = 30;
 sz = size(img);
 min_pixel = ceil(512*512/1000);
 R = zeros(sz(1:2));
@@ -15,14 +15,16 @@ for m = 1:sz(1)
     for n = 1:sz(2)
         row_combine = false; col_combine = false;
         % compare by rc and cc
-        pixel = squeeze(img(m,n,:))';
+        pixel = squeeze(img(m,n,:))'; up_region = 0; left_region = 0;
         if n ~= 1
-            if mean(abs(pixel - m_n_list(R(m,n-1),1:3))) <= th
+            left_region = R(m,n-1); leftdiff = pixel - m_n_list(left_region,1:3);
+            if mean(abs(leftdiff)) <= th
                 col_combine = true;
             end
         end
         if m ~= 1
-            if mean(abs(pixel - m_n_list(R(m-1,n),1:3))) <= th
+            up_region = R(m-1,n); updiff = pixel - m_n_list(up_region,1:3);
+            if mean(abs(updiff)) <= th
                 row_combine = true;
             end
         end
@@ -33,37 +35,35 @@ for m = 1:sz(1)
         if row_combine && col_combine   % top and left are the same region
             % update mean, num and region combine
             % if regions are the same
-            if R(m-1,n) == R(m,n-1)
-                flag = 1; ref_point = R(m-1,n);
+            if up_region == left_region
+                flag = 1; ref_point = up_region;
             else
-                if R(m-1,n) > R(m,n-1)  % R(m-1,n) combine to R(m,n-1)
-                    flag = 2; ref_point = R(m,n-1); replace_point = R(m-1,n);
-                else    % R(m,n-1) combine to R(m-1,n)
-                    flag = 2; ref_point = R(m-1,n); replace_point = R(m,n-1);
+                if mean(abs(updiff - leftdiff)) <= th
+                    flag = 2; ref_point = min(up_region,left_region); replace_point = max(up_region,left_region);
+                elseif updiff > leftdiff
+                    flag = 1; ref_point = left_region;
+                else
+                    flag = 1; ref_point = up_region;
                 end
             end    
         elseif col_combine
-            flag = 1; ref_point = R(m,n-1);
+            flag = 1; ref_point = left_region;
         elseif row_combine
-            flag = 1; ref_point = R(m-1,n);
+            flag = 1; ref_point = up_region;
         else
+            r_cnt = r_cnt+1; ref_point = r_cnt; flag = 0;
         end
+        R(m,n) = ref_point;
         switch flag
             case 0  % add new region
-                r_cnt = r_cnt + 1;
-                R(m,n) = r_cnt;
-                
                 m_n_list = cat(1,m_n_list,[pixel,1]);
-                
             case 1  % add one new point
-                R(m,n) = ref_point;
                 new_num = m_n_list(ref_point,4) + 1;
                 new_mean = (m_n_list(ref_point,1:3).* m_n_list(ref_point,4) + pixel)./ new_num;
                 m_n_list(ref_point,:) = [new_mean, new_num];
             case 2  % combine one region to another
-                R(m,n) = ref_point;
-                new_num = m_n_list(ref_point,4) + m_n_list(replace_point,4) + 1;
-                new_mean = (m_n_list(ref_point,1:3).*m_n_list(ref_point,4) + m_n_list(replace_point,1:3).*m_n_list(replace_point,4) + pixel) / new_num;
+                new_num = m_n_list(ref_point,end) + m_n_list(replace_point,end) + 1;
+                new_mean = (m_n_list(ref_point,1:3).*m_n_list(ref_point,end) + m_n_list(replace_point,1:3).*m_n_list(replace_point,end) + pixel) / new_num;
                 m_n_list(ref_point,:) = [new_mean, new_num];
                 m_n_list(replace_point,:) = zeros(1,4);
                 R(R == replace_point) = ref_point;
@@ -86,15 +86,14 @@ for i = 1:length(index)
     di_M(conv2(M,neighbor_matrix,'same') > 0) = 1;
     [n_r,n_c] =  find((di_M - M) == 1); % get neighbor
     u = unique(R((n_c-1)*sz(1)+n_r));
-    if ~isempty(u(m_n_list(u,4) > min_pixel))
-        u = u(m_n_list(u,4) > min_pixel);
+    if ~isempty(u(m_n_list(u,end) > min_pixel))
+        u = u(m_n_list(u,end) > min_pixel);
     end
 %     [min_dist,u_index]=min((abs(mean_num_list(index(i),1) - mean_num_list(u,1))./sqrt(mean_num_list(u,2))));
     [min_dist,u_index]=min(mean(abs(m_n_list(index(i),1:3) - m_n_list(u,1:3)),2));
     % update mean, num, region
     region_index = u(u_index(1));
-    new_num = m_n_list(region_index,4) + m_n_list(index(i),4);
-    m_n_list(region_index,4) = new_num;
+    m_n_list(region_index,end) = m_n_list(region_index,end) + m_n_list(index(i),end);
     R(R == index(i)) = region_index;
     m_n_list(index(i),:) = zeros(1,4);
 end
