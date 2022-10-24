a8064a514563d3f8caa89997b70ed12c17a1f923
ray-qwer <b07901055@ntu.edu.tw>
2022-10-24 12:12:50 +0800

    read some files

--- ERS/testERS.m
+++ ERS/testERS.m
@@ -1,10 +1,11 @@
-name = '37073';
+name = 'lena_color';
 path = '';
-imgPath = strcat(name, '.jpg');
+imgPath = strcat(name, '.png');
 oriImg = imread(imgPath);
 grey_img = double(rgb2gray(oriImg));
 [h,w]=size(grey_img);
 nC = floor(w*h/200);
+% nC = 40;
 t = cputime;
 segments = mex_ers(grey_img,nC);
 edge=(segments~=segments(:,[1,1:w-1])) | (segments~=segments([1,1:h-1],:));
